b7c942b74f24fb707babbb026c0f8d607aaf8133
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2014-03-22 09:33:49 +0100

    Update wpdk-sample-plugin-4.php

--- wpdk-sample-plugin-4.php
+++ wpdk-sample-plugin-4.php
@@ -13,8 +13,12 @@
 
 // Include WPDK framework - the root directory name of WPDK may be different.
 // Please change the line below according to your environment.
-//require_once( trailingslashit( dirname( dirname( __FILE__ ) ) ) . 'wpdk-production/wpdk.php' );
-require_once( trailingslashit( dirname( dirname( __FILE__ ) ) ) . 'wpxtreme/wpdk/wpdk.php' );
+if ( file_exists( trailingslashit( dirname( dirname( __FILE__ ) ) ) . 'wpxtreme/wpdk/wpdk.php' ) ) {
+  require_once( trailingslashit( dirname( dirname( __FILE__ ) ) ) . 'wpxtreme/wpdk/wpdk.php' );
+} else {
+  require_once( 'wpdk/wpdk.php' );
+}
+
 
 /**
  * WPDKSamplePlugin is the main class of this plugin, and extends WPDKWordPressPlugin
